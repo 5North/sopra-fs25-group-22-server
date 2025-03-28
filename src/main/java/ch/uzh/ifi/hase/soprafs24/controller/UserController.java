cda18653a8eb9be89e77959b148161945971a1ef
5North <199603038+5North@users.noreply.github.com>
2025-03-28 13:46:33 +0100

    Remove unused import

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/UserController.java
@@ -2,7 +2,6 @@ package ch.uzh.ifi.hase.soprafs24.controller;
 
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.UserGetDTO;
-import ch.uzh.ifi.hase.soprafs24.rest.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.UserService;
 import org.springframework.http.HttpHeaders;
