e9c274d7a43350a6011a0ebf886a4325ede17386
5North <199603038+5North@users.noreply.github.com>
2025-05-05 12:59:24 +0200

    Revert "Add docker pkg" [skip ci]
    This reverts commit fbdf1308cf2505a5bbcb7940311a5705b5817c34.

--- flake.nix
+++ flake.nix
@@ -29,7 +29,6 @@
           [
             jdk
             git
-            docker
           ]
           ++ lib.optionals stdenv.isDarwin [
             xcodes
@@ -47,7 +46,6 @@
             
             export PATH="${pkgs.jdk}/bin:$PATH"
             export PATH="${pkgs.git}/bin:$PATH"
-            export PATH="${pkgs.docker}/bin:$PATH"
             
             XCODE_VERSION_OLD="15.3"
             XCODE_VERSION="16.2"
